41b2aee70a55ede8fa4577658b94bbd1f4897574
SamandarXudoynazarov <samandarxudoynazarovno2@gmail.com>
2024-12-22 12:52:59 +0500

    Sizning commit izohi bu yerda

---
+++ JS/data.js
@@ -0,0 +1,94 @@
+// data.js
+export const data = [
+    {
+      id: 1,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Uhaqida malumot",
+      detalis: "U haqida koproq malumot",
+    },
+    {
+      id: 2,
+      title: "Uztozning ism familyasi ",
+      image: "../Asisent/img/images.jpg",
+      description: "Uhaqida malumot",
+    },
+    {
+      id: 3,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 3 ning tavsifi",
+    },
+    {
+      id: 4,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 4 ning tavsifi",
+    },
+    {
+      id: 5,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 5 ning tavsifi",
+    },
+    {
+      id: 6,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 6 ning tavsifi",
+    },
+    {
+      id: 7,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 7 ning tavsifi",
+    },
+    {
+      id: 8,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 8 ning tavsifi",
+    },
+    {
+      id: 9,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 9 ning tavsifi",
+    },
+    {
+      id: 10,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 10 ning tavsifi",
+    },
+    {
+      id: 11,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 11 ning tavsifi",
+    },
+    {
+      id: 12,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 12 ning tavsifi",
+    },
+    {
+      id: 13,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 13 ning tavsifi",
+    },
+    {
+      id: 14,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 14 ning tavsifi",
+    },
+    {
+      id: 15,
+      image: "../Asisent/img/images.jpg",
+      title: "Uztozning ism familyasi ",
+      description: "Bu Uztozning ism familyasi 15 ning tavsifi",
+    },
+];
